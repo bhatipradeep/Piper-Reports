362be292f300a278c8f090055ed84e0de5e98bc9
Pradeepsingh Bhati <bh.pradeepsingh@gmail.com>
2020-06-20 20:01:36 +0530

    update Auth0

--- main/src/Auth0.js
+++ main/src/Auth0.js
@@ -4,9 +4,9 @@ import auth0 from 'auth0-js'
 class Auth {
     constructor(){
         this.auth0 = new auth0.WebAuth({
-            domain: '',
-            audience: '',
-            clientID: '',
+            domain: process.env.REACT_APP_AUTH0_DOMAIN,
+            audience: 'https://'+process.env.REACT_APP_AUTH0_DOMAIN+'/userinfo',
+            clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
             redirectUri: 'http://localhost:3000/callback',
             responseType: 'id_token',
             scope: 'openid profile'
